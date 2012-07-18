cd0a82c76ee727074eb0c76b0b850a7c973b30df
nardele <nardele@Einstein.(none)>
2012-07-18 00:00:33 -0300

    Clone do Githubempty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineSeller::Application.config.secret_token = '7fedb7cc4e4704f8c1d202306d55337a580b4622fb67d2cd0e41b9d02b18f8946ce99616f626be47181c00a38fc24537d3767f233fac3fdebaaf00416ce9bc36'
