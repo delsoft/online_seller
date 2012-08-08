83a705a7ac4b8421edf211ddf251977666dc60a3
Nardele Salomon <noliveira@telelistas.net>
2012-08-08 15:17:39 -0300

    teste

---
+++ lib/telelistas.rb
@@ -0,0 +1,15 @@
+
+class Teste
+  def teste
+    "123"
+  end
+end
+
+require 'well_formatted/act_as_well_formatted'
+
+
+module Telelistas
+end
+
+puts "==========================================++"
+
